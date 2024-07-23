1cd86852a275e38724b566282a4a40a6f4cd21d4
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-23 16:36:54 -0600

    Added changes to the factory bot

--- spec/factories/invoice_item_factory.rb
+++ spec/factories/invoice_item_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invoice_item do
     item
     invoice
-    quantity { rand(1..10) }
+    quantity { rand(1..20) }
     unit_price { Faker::Number.number(digits: 4)}
     status { rand(0..2) }
   end
