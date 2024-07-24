3b4cbf77c5fdeb97b8ca2a5f6b3fbb7504ca4541
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-24 14:22:51 -0600

    Add model relationship and validation tests for the invoice_spec.rb file

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Invoice, type: :model do
+  describe "relationships" do
+    it { should belong_to(:customer) }
+    it { should have_many(:transactions) }
+    it { should have_many(:invoice_items) }
+    it { should have_many(:items).through(:invoice_items) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:rank) }
+    it { should validate_numericality_of(:rank) }
+  end
+end
