fa18f0f822e0f3725694398c3b4d4a54a39f9a84
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-24 15:08:23 -0600

    Add changes  to validations to change enum column to presence of instead of numericality

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   validates :credit_card_number, presence: true, numericality: true  
   validates :credit_card_expiration_date, presence: true 
-  validates :result, presence: true, numericality: true 
+  validates :result, presence: true
   
   belongs_to :invoice
   
