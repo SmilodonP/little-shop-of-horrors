7632eb3e0a61630bec7d12884a3b77e4701cc152
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-24 14:20:11 -0600

    Add validations in the invoice_item model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,12 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  validates_presence_of :item_id 
+  validates_presence_of :invoice_id
+  validates_numericality_of :quantity
+  validates_numericality_of :unit_price
+  validates_numericality_of :status
   
   enum status: {
     pending 0,
