9c2592301716af11d99ddd097115dbdfaa7207fb
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-24 14:50:50 -0600

    Refactor the relationships in the customer.rb model to jsut having many invoices

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,5 @@
 class Customer < ApplicationRecord
   has_many :invoices
-  has_many :items, through: :invoices
-  has_many :merchants, through: :items
-  has_many :items
 
   validates_presence_of :first_name
   validates_presence_of :last_name
