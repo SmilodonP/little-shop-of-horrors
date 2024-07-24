eccc8937f5a975a8d70cb5064afd844cdd35ac27
Cory Bretsch <corybretsch@gmail.com>
2024-07-24 15:40:20 -0500

    adding 3 model tests - currently not passing

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
+  validates :credit_card_number, presence: true, numericality: true  
+  validates :credit_card_expiration_date, presence: true 
+  validates :result, presence: true, numericality: true 
+  
   belongs_to :invoice
   
   enum result: {
