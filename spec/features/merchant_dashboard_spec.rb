f5ae28da81c4fb1b0952a1f90ddebfe563bf1e20
James-Cochran <jameslikesfruit@gmail.com>
2024-07-25 14:00:29 -0400

    started making customer and invoice instances for user story 3

--- spec/features/merchant_dashboard_spec.rb
+++ spec/features/merchant_dashboard_spec.rb
@@ -4,6 +4,34 @@ RSpec.describe "Merchant Dashboard" do
   before(:each) do
     @merchant_1 = create(:merchant, name: "Seymore")
     @merchant_2 = create(:merchant, name: "Audrey")
+
+    @customer_1 = create(:customer, name: "Undertaker")
+    @customer_2 = create(:customer, name: "Mankind")
+    @customer_3 = create(:customer, name: "Mick Foley")
+    @customer_4 = create(:customer, name: "Cactus Jack")
+    @customer_5 = create(:customer, name: "Dude Love")
+    @customer_6 = create(:customer, name: "Rick Flair")
+
+    @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_2)
+    @invoice_2 = create(:invoice, merchant: @merchant_1, customer: @customer_3)
+    @invoice_3 = create(:invoice, merchant: @merchant_1, customer: @customer_3)
+    @invoice_4 = create(:invoice, merchant: @merchant_1, customer: @customer_4)
+    @invoice_5 = create(:invoice, merchant: @merchant_1, customer: @customer_4)
+    @invoice_6 = create(:invoice, merchant: @merchant_1, customer: @customer_4)
+    @invoice_7 = create(:invoice, merchant: @merchant_1, customer: @customer_5)
+    @invoice_8 = create(:invoice, merchant: @merchant_1, customer: @customer_5)
+    @invoice_9 = create(:invoice, merchant: @merchant_1, customer: @customer_5)
+    @invoice_10 = create(:invoice, merchant: @merchant_1, customer: @customer_5)
+    @invoice_11 = create(:invoice, merchant: @merchant_1, customer: @customer_6)
+    @invoice_12 = create(:invoice, merchant: @merchant_1, customer: @customer_6)
+    @invoice_13 = create(:invoice, merchant: @merchant_1, customer: @customer_6)
+    @invoice_14 = create(:invoice, merchant: @merchant_1, customer: @customer_6)
+    @invoice_15 = create(:invoice, merchant: @merchant_1, customer: @customer_6)
+
+
+    
+    
+
   end
   
   # User Story 1
@@ -28,4 +56,7 @@ RSpec.describe "Merchant Dashboard" do
     #save_and_open_page
     expect(page).to have_link("Invoices Index")
   end
+
+  # User Story 3
+  
 end
