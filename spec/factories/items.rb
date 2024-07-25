7e53641cd910746c13d8c6f1e347febd364f6df6
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-07-25 14:29:58 -0600

    Update items model & customer model method

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :item do
     name { Faker::Name.name }
     description { Faker::Commerce.material }
-    unit_price { Faker::Commerce.price }
+    unit_price { Faker::Number.between(1..1000000)} # Might want to try 2 methods
+    # unit_price { Faker::Number.between(from:1, to: 1000000)} # One of these should work
     merchant { association :merchant }
   end
 end
