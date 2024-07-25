86cba1fe571648690f0acbf53dac68562ce82b55
James-Cochran <jameslikesfruit@gmail.com>
2024-07-24 21:03:25 -0400

    correction to the rspec test path, created dashboard view, passing test.

--- spec/features/merchant_dashboard_spec.rb
+++ spec/features/merchant_dashboard_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe "Merchant Dashboard" do
   before(:each) do
     @merchant = create(:merchant)
   end
+  
   # User Story 1
   it "Has the name of the merchant when I visit that merchants dashboard" do
     visit "/merchants/#{@merchant.id}/dashboard"
 
-    expect(page).to have_content(@merchant_1.name)
+      within("#merchant") do 
+        expect(page).to have_content(@merchant.name)
+    end
   end
 end
