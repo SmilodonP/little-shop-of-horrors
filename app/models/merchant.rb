3f7dbda7c649dacd33dc6cfe1be68e8bbc54dfc8
Karl Fallenius <smilodon.p@gmail.com>
2024-07-26 17:36:12 -0600

    working through US 3

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,14 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
 
   has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: invoice_items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoice_items
+  
+  # def top_customers
+  #   Customer.select("customers.id, customers.first_name, customers.last_name, count(transactions.id) as top_customers").joins(invoices: :transactions).where(transactions: {result: 1}, invoices: {merchant_id: 1}).group("customers.id, customers.first_name, customers.last_name").order("top_customers desc").limit(5)
+  # end
+  @top_5_customers = Customer.top_5_customers
+
 end
