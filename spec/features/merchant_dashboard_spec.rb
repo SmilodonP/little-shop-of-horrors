358bd82223d07d913616bb772ef797a6328dd57c
James-Cochran <jameslikesfruit@gmail.com>
2024-07-26 19:38:09 -0400

    stupid copy paste issues

--- spec/features/merchant_dashboard_spec.rb
+++ spec/features/merchant_dashboard_spec.rb
@@ -50,13 +50,12 @@ RSpec.describe "Merchant Dashboard" do
     visit "/merchants/#{@merchant_1.id}/dashboard"
 
       within("#merchant") do 
-        expect(page).to have_content("#{@customer_1.first_name} #{@customer_1.last_name}")
-        expect(page).to_not have_content("#{@merchant_2.first_name} #{@customer_2.last_name}")
+        expect(page).to have_content("#{@merchant_1.name}")
+        expect(page).to_not have_content("#{@merchant_2.name}")
     end
   end
 
   # User Story 2
-  it "Has a link to the merchant items index" do
     visit "/merchants/#{@merchant_1.id}/dashboard"
 
     expect(page).to have_link("Items Index")
