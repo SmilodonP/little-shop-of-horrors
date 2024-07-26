f1d9843b58c48d6f7e2c5aa80f256b801825f6fe
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-07-25 19:59:17 -0600

    US#22 complete, invoice model test not 100% covered - revisit

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe Invoice, type: :model do
     it { should validate_presence_of(:customer_id) }
     it { should validate_presence_of(:status) }
   end
+
+  describe "#unshipped_invoices" do
+    it "returns id #'s of unshipped invoices" do
+      customer_1 = create(:customer)
+      customer_3 = create(:customer)
+      customer_5 = create(:customer)
+      merchant_1 = create(:merchant, name: "Tarzhay")
+      invoice_1 = create(:invoice, customer_id: customer_1.id)
+      invoice_3 = create(:invoice, customer_id: customer_3.id)
+      invoice_5 = create(:invoice, customer_id: customer_5.id)
+      item_1 = create(:item, unit_price: 10, merchant_id: merchant_1.id)
+      invoice_item_1 = create(:invoice_item, quantity: 40, unit_price: 10, item_id: item_1.id, invoice_id: invoice_1.id, status: 0)
+      invoice_item_3 = create(:invoice_item, quantity: 15, unit_price: 10, item_id: item_1.id, invoice_id: invoice_3.id, status: 1)
+      invoice_item_5 = create(:invoice_item, quantity: 1, unit_price: 10, item_id: item_1.id, invoice_id: invoice_5.id, status: 2)
+
+      # Shouldn't #unshipped_invoices return the id #'s without 'pluck'?
+      expect(Invoice.unshipped_invoices.pluck(:id)).to eq([invoice_1.id, invoice_3.id])
+    end
+  end
 end
