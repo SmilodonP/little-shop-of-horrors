67104d7b11232ffba9be702d5163083cab2fdb3a
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-28 09:23:10 -0600

    Re-arange the flow control in the update action so that merchants can have their status updated

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -13,19 +13,22 @@ class Admin::MerchantsController < ApplicationController
 
   def update
     @merchant = Merchant.find(params[:id])
-
-    if @merchant.update(merchant_params)
+    
+    if params[:status] #moved this above so that it can correctly enable merchants? Seems strange
+      @merchant.update(status: params[:status])
+      redirect_to admin_merchants_path, notice: "The merchant has been #{@merchant.status}."
+    elsif @merchant.update(name: params[:name])
       redirect_to admin_merchant_path(@merchant), notice: "GREAT SUCCESS!"
-    elsif @merchant.update(status: params[:status])
-      redirect_to admin_merchants_path
     else   
       render :edit
     end
+
+    # if @merchant.update(merchant_params)
+    #   redirect_to admin_merchant_path(@merchant), notice: "GREAT SUCCESS!"
+    # elsif @merchant.update(status: params[:status])
+    #   redirect_to admin_merchants_path
+    # else   
+    #   render :edit
+    # end
   end
-
-    private
-
-    def merchant_params
-      params.require(:merchant).permit(:name)
-    end
 end
