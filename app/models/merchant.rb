a1ffae5313e4f9adbb1f1f44df90bcfaf43edaa8
Karl Fallenius <smilodon.p@gmail.com>
2024-07-27 19:18:22 -0600

    committing to merge this branch with OG james_user_story3 branch

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,12 +6,17 @@ class Merchant < ApplicationRecord
 
   def top_customers
     Customer
-    .select("customers.id, customers.first_name, customers.last_name, count(transactions.id) as top_customers")
-    .joins(invoices: { invoice_items: :item})
+    .select("customers.id, customers.first_name, customers.last_name, count(transactions.id) as transaction_count")
+    .joins(invoices: :transactions)
     .joins('INNER JOIN transactions ON transactions.invoice_id = invoices.id')
+    .joins('INNER JOIN transactions ON items.id = invoice_items.item_id')
     .where(transactions: { result: 1 }, items: { merchant_id: self.id })
     .group("customers.id, customers.first_name, customers.last_name")
     .order("top_customers desc")
     .limit(5)
+
+    puts "#{customer.first_name} #{customer.last_name}: #{customer.transaction_count}"
   end
-end
+  
+  
+end 
