d78d0da86cbe3318522bd936345590814bb68c93
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-28 08:54:58 -0600

    Remove the nested merchants from within invoices and move it to the admin resouces block

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
 
   namespace :admin do
-    resources :invoices, only: [:show] do 
-      resources :merchants, only: [:show, :edit, :update, :index]
-    end
+    resources :invoices, only: [:show]
+    resources :merchants, only: [:show, :edit, :update, :index]
   end
 end
