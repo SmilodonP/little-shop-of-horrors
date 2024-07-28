a87a54a90caa6472bcc23652228780fdb9b6d192
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-28 16:07:44 -0600

    Add test data and a test that returns the top five customers for a particular merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,8 +10,47 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe "model method tests" do
+    before :each do
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
+      @customer_3 = create(:customer)
+      @customer_4 = create(:customer)
+      @customer_5 = create(:customer)
+      @customer_6 = create(:customer)
+
+      @invoice_1 = create(:invoice, customer_id: @customer_1.id)
+      @invoice_2 = create(:invoice, customer_id: @customer_2.id)
+      @invoice_3 = create(:invoice, customer_id: @customer_3.id)
+      @invoice_4 = create(:invoice, customer_id: @customer_4.id)
+      @invoice_5 = create(:invoice, customer_id: @customer_5.id)
+      @invoice_6 = create(:invoice, customer_id: @customer_6.id)
+
+      @transaction_1 = create(:transaction, invoice_id: @invoice_1.id, result: 1)
+      @transaction_2 = create(:transaction, invoice_id: @invoice_2.id, result: 1)
+      @transaction_3 = create(:transaction, invoice_id: @invoice_3.id, result: 1)
+      @transaction_4 = create(:transaction, invoice_id: @invoice_4.id, result: 1)
+      @transaction_5 = create(:transaction, invoice_id: @invoice_5.id, result: 1)
+      @transaction_6 = create(:transaction, invoice_id: @invoice_6.id, result: 0)
+
+      @merchant_1 = create(:merchant, name: "Tarzhay")
+
+      @item_1 = create(:item, unit_price: 10, merchant_id: @merchant_1.id)
+      @item_2 = create(:item, unit_price: 5, merchant_id: @merchant_1.id)
+
+      @invoice_item_1 = create(:invoice_item, quantity: 40, unit_price: 10, item_id: @item_1.id, invoice_id: @invoice_1.id, status: 0)
+      @invoice_item_2 = create(:invoice_item, quantity: 9, unit_price: 10, item_id: @item_1.id, invoice_id: @invoice_2.id, status: 0)
+      @invoice_item_3 = create(:invoice_item, quantity: 15, unit_price: 10, item_id: @item_1.id, invoice_id: @invoice_3.id, status: 1)
+      @invoice_item_4 = create(:invoice_item, quantity: 21, unit_price: 10, item_id: @item_1.id, invoice_id: @invoice_4.id, status: 1)
+      @invoice_item_5 = create(:invoice_item, quantity: 1, unit_price: 10, item_id: @item_1.id, invoice_id: @invoice_5.id, status: 2)
+      @invoice_item_6 = create(:invoice_item, quantity: 8, unit_price: 5, item_id: @item_2.id, invoice_id: @invoice_6.id, status: 2)
+    end
+
     describe "class methods" do
-      
+      describe "top_customers" do
+        it "returns the top five customers for a particular merchant" do
+          expect(merchant_1.top_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
+        end
+      end
     end
 
     describe "instance methods" do
