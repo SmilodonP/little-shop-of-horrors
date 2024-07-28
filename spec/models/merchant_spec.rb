f1350259ef8326632c2cf67bb2f15266ec333a0d
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-28 16:13:40 -0600

    Add fixes to the merchant_spec.rb file

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe Merchant, type: :model do
     describe "class methods" do
       describe "top_customers" do
         it "returns the top five customers for a particular merchant" do
-          expect(merchant_1.top_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
+          expect(@merchant_1.top_five_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
         end
       end
     end
