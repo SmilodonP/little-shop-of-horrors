3a8283c01e4c3b275b5a3571b73c70167ccd8926
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-28 15:45:50 -0600

    Refactor the :id for the params hash to the :merchant_id{

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
     @top_customers = @merchant.top_customers
   end
 end
