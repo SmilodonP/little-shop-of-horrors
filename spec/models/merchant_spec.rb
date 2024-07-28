57883c684441ac239dc61622dab7c665b324f0e8
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-28 15:52:43 -0600

    Add model method tests sections in merchant_spec.rb file

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:items)}
   end
 
+  describe "model method tests" do
+    describe "class methods" do
+      
+    end
+
+    describe "instance methods" do
+
+    end
+  end
 end
