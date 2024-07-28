e61a2d2c3fc7b7aff99f628388486c16929cc496
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-28 16:03:05 -0600

    Add changes to the top_customers name making it @top_five_customers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @top_customers = @merchant.top_customers
+    @top_five_customers = @merchant.top_five_customers
   end
 end
