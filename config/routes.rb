91a9a9f3be517bf06f17777cf279d5576736fd0d
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-29 14:15:37 -0600

    Add resources route for merchant/:id/items/:id path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :merchants, only: [:show, :index] do
     resources :dashboard, only: [:index]
-    resources :items, only: [:index]
+    resources :items, only: [:index, :show]
     resources :invoices, only: [:index]
   end
 
