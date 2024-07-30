59bfd04212b82ced07f187a85b503fcf064e8e7a
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-29 19:34:49 -0600

    Merge US 6 functionality from Lito

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -3,7 +3,7 @@ class MerchantItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @merchant_item = @merchant.items.find(params[:id])
   end
-end
+
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @items = @merchant.items
