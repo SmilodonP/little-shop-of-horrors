48e1ffb3fb0b33818ddd7d0418a7927206e3b17e
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-30 13:28:00 -0600

    Re-arrange order of conditionals in update action

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -17,12 +17,12 @@ class MerchantItemsController < ApplicationController
   def update
     @merchant = Merchant.find(params[:merchant_id])
     @item = @merchant.items.find(params[:id])
-    if @item.update(item_params)
+    if params[:status]
+    @item.update(status: params[:id])
+    redirect_to merchant_items_path(@merchant), noticel: "GREAT SUCCESS! The merch has been #{@item.status}."
+    elsif @item.update(item_params)
       flash[:success] = "Item Info Successfully Updated"
       redirect_to merchant_items_path(@merchant)
-    elsif params[:status]
-      @item.update(status: params[:id])
-      redirect_to merchant_items_path(@merchant), noticel: "GREAT SUCCESS! The merch has been #{@item.status}."
     else
       render :edit
     end
