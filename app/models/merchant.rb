2a5eafc93ea69ca97b60506cef5bad1cce3d3172
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-30 22:23:51 -0600

    Added another query possibility

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -17,17 +17,26 @@ class Merchant < ApplicationRecord
     #   .order("COUNT(transactions.id) DESC")
     #   .limit(5)
     
-    
     customers
-    .select("customers.id, customers.first_name, customers.last_name, COUNT(t1.id) AS transaction_count")
-    .joins('INNER JOIN invoices ON invoices.customer_id = customers.id')
-    .joins('INNER JOIN transactions t1 ON t1.invoice_id = invoices.id')
-    .joins('INNER JOIN invoice_items ON invoices.id = invoice_items.invoice_id')
-    .joins('INNER JOIN items ON items.id = invoice_items.item_id')
-    .where(t1: { result: 1 }, items: { merchant_id: self.id })
-    .group('customers.id, customers.first_name, customers.last_name')
-    .order('transaction_count DESC')
+    .select("customers.id, customers.first_name, customers.last_name, count(transactions.id) as transaction_count")
+    .joins(invoices: :transactions)
+    .joins("INNER JOIN transactions ON transactions.invoice_id = invoices.id")
+    .joins("INNER JOIN items ON items.id = invoice_items.item_id")
+    .where(transactions: { result: 1 }, items: { merchant_id: self.id })
+    .group("customers.id, customers.first_name, customers.last_name")
+    .order("transaction_count desc")
     .limit(5)
+
+    # customers
+    # .select("customers.id, customers.first_name, customers.last_name, COUNT(t1.id) AS transaction_count")
+    # .joins('INNER JOIN invoices ON invoices.customer_id = customers.id')
+    # .joins('INNER JOIN transactions t1 ON t1.invoice_id = invoices.id')
+    # .joins('INNER JOIN invoice_items ON invoices.id = invoice_items.invoice_id')
+    # .joins('INNER JOIN items ON items.id = invoice_items.item_id')
+    # .where(t1: { result: 1 }, items: { merchant_id: self.id })
+    # .group('customers.id, customers.first_name, customers.last_name')
+    # .order('transaction_count DESC')
+    # .limit(5)
   end
 
   def items_ready_to_ship
