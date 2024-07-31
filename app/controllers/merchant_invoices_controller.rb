36a4591a26c69f0a6c617086f156267c4206a7d7
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-30 21:09:44 -0600

    Fix of instance variable names from singular plural for index and show actions

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,12 +1,12 @@
 class MerchantInvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @merchant_invoice = @merchant.invoices
+    @merchant_invoices = @merchant.invoices
   end
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @merchant_invoices = @merchant.invoices.find(params[:id])
+    @merchant_invoice = @merchant.invoices.find(params[:id])
   end
 
 end
