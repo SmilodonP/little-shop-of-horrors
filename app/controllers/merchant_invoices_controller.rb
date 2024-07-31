4fa71380d4afb3f80c046e6fd6b5c6bb3a415187
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-30 20:53:18 -0600

    Add merchant_invoices_controller.rb file with empty index and show actions

---
+++ app/controllers/merchant_invoices_controller.rb
@@ -0,0 +1,11 @@
+class MerchantInvoicesController < ApplicationController
+  def show
+    # @merchant = Merchant.find(params[:merchant_id])
+    # @merchant_invoices = @merchant.invoices.find(params[:id])
+  end
+
+  def index
+    # @merchant = Merchant.find(params[:merchant_id])
+    # @items = @merchant.items
+  end
+end
