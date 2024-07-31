f1fa09ac6738c8a1a357914ab1123f434a5f6a8b
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-30 20:55:48 -0600

    Add instance variables inside the merchant_invoices_controller.rb file

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,11 +1,12 @@
 class MerchantInvoicesController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @items = @merchant.items
+  end
+  
   def show
-    # @merchant = Merchant.find(params[:merchant_id])
-    # @merchant_invoices = @merchant.invoices.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @merchant_invoices = @merchant.invoices.find(params[:id])
   end
 
-  def index
-    # @merchant = Merchant.find(params[:merchant_id])
-    # @items = @merchant.items
-  end
 end
