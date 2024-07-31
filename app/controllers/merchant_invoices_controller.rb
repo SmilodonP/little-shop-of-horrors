555b2017732004f8698a7352e710493583b8aa26
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-30 20:58:58 -0600

    Fix instance variable names for invoices instead of items in the merchants invoices controller

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,9 +1,9 @@
 class MerchantInvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @items = @merchant.items
+    @merchant_invoice = @merchant.items
   end
-  
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @merchant_invoices = @merchant.invoices.find(params[:id])
