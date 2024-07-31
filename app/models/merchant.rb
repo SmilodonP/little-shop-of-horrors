0deea9e20400ca6ba6d4a12faf6e37b407731654
Karl Fallenius <smilodon.p@gmail.com>
2024-07-31 12:32:09 -0600

    adds us3 and us4 working  functionality

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,6 +10,14 @@ class Merchant < ApplicationRecord
   enum status: {disabled: 0, enabled: 1}
 
   def top_five_customers
+    Customer
+      .select("customers.id, customers.first_name, customers.last_name, COUNT(transactions.id) AS transaction_count")
+      .joins(invoices: [:transactions, { invoice_items: :item }])
+      .where(transactions: { result: 1 }, items: { merchant_id: id })
+      .group('customers.id')
+      .order(transaction_count: :desc)
+      .limit(5)
+    
     # customers.joins(invoices: :transactions)
     #   .select("customers.*, COUNT(transactions.id) as transactions")
     #   .where(transactions: {result: 0})
@@ -17,15 +25,15 @@ class Merchant < ApplicationRecord
     #   .order("COUNT(transactions.id) DESC")
     #   .limit(5)
     
-    customers
-    .select("customers.id, customers.first_name, customers.last_name, count(transactions.id) as transaction_count")
-    .joins(invoices: :transactions)
-    .joins("INNER JOIN transactions ON transactions.invoice_id = invoices.id")
-    .joins("INNER JOIN items ON items.id = invoice_items.item_id")
-    .where(transactions: { result: 1 }, items: { merchant_id: self.id })
-    .group("customers.id, customers.first_name, customers.last_name")
-    .order("transaction_count desc")
-    .limit(5)
+    # customers
+    # .select("customers.id, customers.first_name, customers.last_name, count(transactions.id) as transaction_count")
+    # .joins(invoices: :transactions)
+    # .joins("INNER JOIN transactions ON transactions.invoice_id = invoices.id")
+    # .joins("INNER JOIN items ON items.id = invoice_items.item_id")
+    # .where(transactions: { result: 1 }, items: { merchant_id: self.id })
+    # .group("customers.id, customers.first_name, customers.last_name")
+    # .order("transaction_count desc")
+    # .limit(5)
 
     # customers
     # .select("customers.id, customers.first_name, customers.last_name, COUNT(t1.id) AS transaction_count")
@@ -40,20 +48,21 @@ class Merchant < ApplicationRecord
   end
 
   def items_ready_to_ship
-    items.joins(invoice_items: :invoice)
-    .select("items.*, invoices.created_at, invoices.id as invoice")
-    .where(invoice_items: 0)
-    .order("invoices.created_at asc")
+    Item
+    .joins(invoice_items: :invoice)
+    .select("items.*, invoices.created_at, invoices.id as invoice_id")
+    .where(invoice_items: {status:0})
+    .order("invoices.created_at")
     .distinct
   end
 
-  def self.top_five_merchants 
-    joins(:transactions)
-    .select("merchants.id, merchants.name, sum(invoice_items.quantity*invoice_items.unit_price) as revenue")
-    .where("transactions.result = 1")
-    .group("merchants.id")
-    .order("revenue desc")
-    .limit(5)
-  end
+  # def self.top_five_merchants 
+  #   joins(:transactions)
+  #   .select("merchants.id, merchants.name, sum(invoice_items.quantity*invoice_items.unit_price) as revenue")
+  #   .where("transactions.result = 1")
+  #   .group("merchants.id")
+  #   .order("revenue desc")
+  #   .limit(5)
+  # end
 
 end
